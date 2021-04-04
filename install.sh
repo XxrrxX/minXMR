5a5b48a93e03fea2951e798ff67cfd6c7cae23d2
XxrrxX <xxrrxx3888@gmail.com>
2021-04-03 19:35:35 -0600

    install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,11 @@ apt-get update && clear && apt-get upgrade -y && clear && apt-get install git ru
 gem install lolcat -y
 clear
 git clone https://github.com/xmrig/xmrig.git && clear
-cd xmrig && mkdir build && cd build && cmake .. && clear && make -j $(nproc) && clear
+cd xmrig 
+mkdir build 
+cd build 
+cmake ..
+clear 
+make -j $(nproc)
+clear
 
