1cf6c2c55421250397cd82a6618e3824bcc1dbea
XxrrxX <xxrrxx3888@gmail.com>
2021-04-03 20:01:57 -0600

    install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
-apt-get update && clear && apt-get upgrade -y && clear && apt-get install git ruby python cmake figlet -y && clear
-gem install lolcat -y
 clear
+apt-get update && clear && apt-get upgrade -y && clear && apt-get install git ruby python cmake figlet wget proot -y && hash -r && clear
+gem install lolcat -y
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/SSH/Apt/ssh-apt.sh && bash ssh-apt.sh
+clear
+cd
 git clone https://github.com/xmrig/xmrig.git && clear
 cd xmrig 
 mkdir build 
 cd build 
-cmake .. -DWITH_HTTTPD=OFF -DWITH_TLS=OFF
+cmake .. -DWITH_HTTPD=OFF -DWITH_TLS=OFF
 clear 
 make -j $(nproc)
 clear
