bf6de30e07fba66beae3a49e539fdeb4a2b9e59b
XxrrxX <xxrrxx3888@gmail.com>
2021-04-04 01:31:17 -0600

    install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@ apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 gem install lolcat -y
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/SSH/Apt/ssh-apt.sh && bash ssh-apt.sh
 clear
-cd
-git clone https://github.com/Bendr0id/xmrigCC && clear
-cd xmrigCC
-mkdir build 
-cd build 
-cmake .. -DWITH_HTTPD=OFF -DWITH_TLS=OFF -DWITH_CUDA=OFF -DWITH_HWLOC=OFF -DWITH_OPENCL=OFF -DCMAKE_BUILD_TYPE=Release
-clear 
-make -j $(nproc)
+rm -r ssh-apt.sh
+cd ..
+git clone https://github.com/xmrig/xmrig
 clear
-
+cd xmrig 
+mkdir build
+cd build
+cmake ..
+make
+clear 
+cd minXMR/ && minXMR.sh
