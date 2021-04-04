8b1d7aa988282e36718b3fd4306a1b4b71b65fc8
XxrrxX <xxrrxx3888@gmail.com>
2021-04-03 20:18:10 -0600

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 clear
 apt-get update && clear && apt-get upgrade -y && clear && apt-get install git ruby python cmake figlet wget proot -y && hash -r && clear
+apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 gem install lolcat -y
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/SSH/Apt/ssh-apt.sh && bash ssh-apt.sh
 clear
