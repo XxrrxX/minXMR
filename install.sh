08768d9c87db14a5b8b4d2370008d27429032968
XxrrxX <xxrrxx3888@gmail.com>
2021-04-04 15:54:47 -0500

    install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,56 @@
 clear
+echo "instalando recursos..."|lolcat -a -d 5
+sleep 5
+clear
 apt-get update && clear && apt-get upgrade -y && clear && apt-get install git ruby python cmake figlet wget proot -y && hash -r && clear
+cd 
+git clone https://github.com/busyloop/lolcat
+cd lolcat/bin && gem install lolcat                             cd && rm -r lolcat                                              clear
 apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-gem install lolcat -y
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/SSH/Apt/ssh-apt.sh && bash ssh-apt.sh
 clear
 rm -r ssh-apt.sh
 cd ..
+clear
+echo "Clonando la herramienta de minado!..."|lolcat -a -d 5
+sleep 5
 git clone https://github.com/xmrig/xmrig
 clear
-cd xmrig 
+cd xmrig
+echo "Creando carpetas..."|lolcat -a -d 5
+sleep 5
 mkdir build
 cd build
+clear
+echo "compilando archivo..."|lolcat -a -d 5
+sleep 5
 cmake ..
 make
 clear 
-cd minXMR/ && minXMR.sh
+cd
+echo "Gracias por ser paciente!"|lolcat -a -d 5
+echo ""
+echo "Todo listo para comenzar a minar ;) [✓]"|lolcat -a -d 5
+echo ""
+echo "Ejecutando minXMR... :D!"|lolcat -a -d 5
+sleep 5
+clear 
+figlet -f slant "5"|lolcat -a -d 1
+sleep 1
+clear
+figlet -f slant "4"|lolcat -a -d 1
+sleep 1
+clear 
+figlet -f slant "3"|lolcat -a -d 1
+sleep 1
+clear 
+figlet -f slant  "2"|lolcat -a -d 1
+sleep 1
+clear
+figlet -f slant "1"|lolcat -a -d 1
+sleep 1
+clear 
+echo "baner"|lolcat -a -d 5
+sleep 3
+clear
+cd minXMR/ && sh minXMR.sh
--- minXMR.sh
+++ minXMR.sh
@@ -1,1 +1,38 @@
+reset
+figlet -f standard "minXMR"|lolcat -a -d 3
+figlet -f slant "by"|lolcat -a -d 3
+figlet -f slant "XxrrxX"|lolcat -a -d 3
+sleep 5
+clear
+echo "Cual es tu nombre ?"|lolcat -a -d 3
+read name
+clear
+figlet -f standard "Hola"|lolcat -a -d 3
+figlet -f slant $name|lolcat -a -d 3
+echo "Todo listo para minar! [✓]"|lolcat -a -d 3
+echo "Excelente miner time!"|lolcat -a -d 3
+sleep 5
+clear
+cd ..
+cd xmrig/build
+clear
+./xmrig -o us-west.minexmr.com:443 -u 41kTojGZCPqZroAUaRPXrm3kho1dMgJGhQLJFZLt3gwYdEka8hx9GpdPbqpnwcPapeSnR3SMN8RaPJ3hzRidYy7UT17B8g -k --tls --rig-id $name
+clear
+echo "Saliendo de minXMR"|lolcat -a -d 3
+echo ""
+echo "Para volver a minar copia y pega la siguiente linea:"|lolcat -a -d 3
+echo ""
+echo ""
+echo "Primero:"|lolcat -a -d 3
+echo ""
+echo "cd && cd ../ubuntu-in-termux/ && ./startubuntu.sh"|lolcat -a -d 3
+echo ""
+echo "Despues en root@localhost:~# pega :"|lolcat -a -d 3 
+echo ""
+echo "cd && cd minXMR && sh minXMR.sh"|lolcat -a -d 3
+echo ""
+echo ""
+figlet -f slant "bye bye!"|lolcat -a -d 3
+echo ""
+echo ""
 
